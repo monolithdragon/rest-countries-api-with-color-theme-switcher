1c996c4e28994d4f05998d119393877f40967cac
Joó Lajos <88922090+monolithdragon@users.noreply.github.com>
2024-05-08 09:06:01 +0200

    Update with ThemeProvider

--- src/layouts/app/index.tsx
+++ src/layouts/app/index.tsx
@@ -1,12 +1,13 @@
 import Header from 'components/header';
 import { Outlet } from 'react-router-dom';
+import ThemeProvider from 'services/providers/theme';
 
 function AppLayout() {
   return (
-    <div>
+    <ThemeProvider options={{ save: true }}>
       <Header />
       <Outlet />
-    </div>
+    </ThemeProvider>
   );
 }
 
