65b03f111896347e5dd5a8e2fc30773f0ce2cda8
Joó Lajos <88922090+monolithdragon@users.noreply.github.com>
2024-05-08 11:45:37 +0200

    Add search functionality to CountrySearch component
    Import Search component and FaMagnifyingGlass icon to implement search
    functionality in the CountrySearch component.

--- src/components/country-search/index.tsx
+++ src/components/country-search/index.tsx
@@ -1,7 +1,15 @@
+import { Search } from 'components/ui/search';
+import { FaMagnifyingGlass } from 'react-icons/fa6';
+
 type Props = {};
 
 function CountrySearch({}: Props) {
-  return <div>CountrySearch</div>;
+  return (
+    <div className="element inline-flex w-full items-center gap-2 rounded px-4 py-4 text-sm lg:w-2/5">
+      <FaMagnifyingGlass />
+      <Search />
+    </div>
+  );
 }
 
 export default CountrySearch;
