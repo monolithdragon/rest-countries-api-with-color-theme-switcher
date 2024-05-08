33d729187dd099e86966573223e68a02dfd7360c
Joó Lajos <88922090+monolithdragon@users.noreply.github.com>
2024-05-08 11:57:56 +0200

    Add regions data to FilterByRegion component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,12 +3,14 @@ import CountrySearch from 'components/country-search';
 import FilterByRegion from 'components/filter-by-region';
 import ContentLayout from 'layouts/content';
 
+const data = ['Africa', 'America', 'Europa'];
+
 function HomePage() {
   return (
     <ContentLayout>
-      <section>
+      <section className="flex flex-wrap justify-between gap-y-10">
         <CountrySearch />
-        <FilterByRegion />
+        <FilterByRegion regions={data} />
       </section>
       <section>
         <CountryList />
