d2f2aa84a6a880299996655b144929b1251a7082
Joó Lajos <88922090+monolithdragon@users.noreply.github.com>
2024-05-09 11:23:05 +0200

    Refactor CountryList component to display data using Card component

--- src/components/country-list/index.tsx
+++ src/components/country-list/index.tsx
@@ -1,7 +1,18 @@
-type Props = {};
+import { Card } from 'components/ui/card';
+import { useData } from 'hooks/use-data';
 
-function CountryList({}: Props) {
-  return <div>CountryList</div>;
+function CountryList() {
+  const { data } = useData();
+
+  return (
+    <div className="flex flex-wrap place-content-center gap-8 sm:gap-[72px]">
+      {data && Array.isArray(data)
+        ? data.map((country: any) => (
+            <Card key={country.name.common} data={country} />
+          ))
+        : data && <Card data={data} />}
+    </div>
+  );
 }
 
 export default CountryList;
