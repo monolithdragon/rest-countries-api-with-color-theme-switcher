77fe726c57e57fd2efb72ff94c418e5d8f00feea
Joó Lajos <88922090+monolithdragon@users.noreply.github.com>
2024-05-09 11:23:14 +0200

    Update Search component test to include navigation on Enter key press
    Add spy on useNavigate and verify navigation on Enter key press event in Search component test.

--- src/components/ui/search/index.test.tsx
+++ src/components/ui/search/index.test.tsx
@@ -1,7 +1,11 @@
-import { fireEvent, renderWithRouter } from 'vitest-setup';
+import { fireEvent, renderWithRouter, waitFor } from 'vitest-setup';
 import { Search } from '.';
+import * as router from 'react-router';
 
 describe('Search component', () => {
+  const navigate = vi.fn();
+  vi.spyOn(router, 'useNavigate').mockImplementation(() => navigate);
+
   it('should update input value on change', () => {
     const { getByRole } = renderWithRouter(<Search />);
     const inputElement = getByRole('searchbox');
@@ -11,13 +15,14 @@ describe('Search component', () => {
     expect(inputElement.getAttribute('value')).toBe('USA');
   });
 
-  it('should clear input value on Enter key press', () => {
+  it('should clear input value on Enter key press', async () => {
     const { getByPlaceholderText } = renderWithRouter(<Search />);
     const inputElement = getByPlaceholderText('Search for a country...');
 
-    fireEvent.change(inputElement, { target: { value: 'USA' } });
+    fireEvent.change(inputElement, { target: { value: 'Hungary' } });
     fireEvent.keyUp(inputElement, { key: 'Enter', code: 'Enter' });
 
     expect(inputElement.getAttribute('value')).toBe('');
+    await waitFor(() => expect(navigate).toHaveBeenCalledWith('../hungary'));
   });
 });
