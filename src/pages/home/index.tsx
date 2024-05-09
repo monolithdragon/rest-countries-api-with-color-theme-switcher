9e42b544d7977249ecd1a63100bf622cca6d9c1f
Joó Lajos <88922090+monolithdragon@users.noreply.github.com>
2024-05-09 11:23:53 +0200

    Refactor region filtering logic in HomePage component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,16 +1,37 @@
 import CountryList from 'components/country-list';
 import CountrySearch from 'components/country-search';
 import FilterByRegion from 'components/filter-by-region';
+import { useData } from 'hooks/use-data';
 import ContentLayout from 'layouts/content';
-
-const data = ['Africa', 'America', 'Europa'];
+import { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 function HomePage() {
+  const [regionName, setRegionName] = useState<Set<string>>(
+    () => new Set<string>(),
+  );
+  const rawData = useLoaderData();
+  const { saveData } = useData();
+
+  useEffect(() => {
+    if (rawData) {
+      saveData(rawData);
+    }
+  }, [rawData]);
+
+  useEffect(() => {
+    if (Array.isArray(rawData)) {
+      rawData.map((item: any) =>
+        setRegionName((prev) => new Set(prev).add(item.region)),
+      );
+    }
+  }, []);
+
   return (
     <ContentLayout>
       <section className="flex flex-wrap justify-between gap-y-10">
         <CountrySearch />
-        <FilterByRegion regions={data} />
+        <FilterByRegion regions={[...regionName]} />
       </section>
       <section className="mt-12">
         <CountryList />
