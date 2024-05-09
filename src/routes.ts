b02465ca8ef770b13bcfadc6a4c44f2992391e47
Joó Lajos <88922090+monolithdragon@users.noreply.github.com>
2024-05-09 08:27:29 +0200

    Refactor route configurations for dynamic data loading
    Update route configurations in routes.ts to include loaders for fetching data dynamically based on route parameters.

--- src/routes.ts
+++ src/routes.ts
@@ -9,8 +9,31 @@ export const routes = createBrowserRouter(
       path: '/',
       element: React.createElement(AppLayout),
       children: [
-        { index: true, element: React.createElement(HomePage) },
-        { path: ':id', element: React.createElement(HomePage) },
+        {
+          index: true,
+          element: React.createElement(HomePage),
+          loader: async () => {
+            return (await fetch('https://restcountries.com/v3.1/all')).json();
+          },
+        },
+        {
+          path: ':id',
+          element: React.createElement(HomePage),
+          loader: async ({ params }) => {
+            return (
+              await fetch(`https://restcountries.com/v3.1/name/${params.id!}`)
+            ).json();
+          },
+        },
+        {
+          path: 'region/:id',
+          element: React.createElement(HomePage),
+          loader: async ({ params }) => {
+            return (
+              await fetch(`https://restcountries.com/v3.1/region/${params.id!}`)
+            ).json();
+          },
+        },
       ],
     },
   ],
